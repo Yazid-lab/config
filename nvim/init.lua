c660d3cf75a553e2e5a6266873572720d2746f34
Yazid <yazidbougrine99@gmail.com>
2023-04-01 22:40:47 +0100

    feat: swapped to nvchad

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,56 +1,21 @@
-require("config")
---MAPPINGS
-local function map(mode, l, r, desc)
-	vim.keymap.set(mode, l, r, { noremap = true, silent = true,desc = desc })
+require "core"
+
+local custom_init_path = vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
+
+if custom_init_path then
+  dofile(custom_init_path)
 end
 
---setting the leader character
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-map("n", "<C-d>", "<C-d>zz", "scroll down half a page and center")
-map("n", "<C-u>", "<C-u>zz",  "scroll up half a page and center")
-map("n", "<Space>", "<NOP>", "unmapping the space bar")
-map("n", "<leader>j", ":Ex<CR>", "Open the netrw menu")
-map("n", "<leader>so", ":so %<cr>", "Source the current file")
-map("n", "<C-s>", ":w<cr>", "Save the current file in normal mode")
-map("i", "<C-s>", "<C-c>:w<cr>a",  "Save the current file in insert mode")
-map("n", "<C-j>", "<C-w>j", "Move down pane")
-map("n", "<C-k>", "<C-w>k",  "Move up pane")
-map("n", "<C-l>", "<C-w>l", "Move left pane")
-map("n", "<C-h>", "<C-w>h", "Move right pane")
-map("n","<leader>v",":vsp<CR>","Split vertically")
-map("n","<leader>h",":sp<CR>","Split horizontally")
-map('n','<leader>x',':q<CR>','close buffer')
---general configs
-local set = vim.opt
-vim.wo.wrap = false
-set.nu = true
-set.relativenumber = true
-vim.opt.expandtab = true
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.smartindent = true
-vim.opt.errorbells = false
-vim.opt.smartindent = true
-vim.opt.swapfile = false
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
-vim.opt.termguicolors = true
-vim.opt.signcolumn = "yes"
-vim.opt.updatetime = 50
-vim.o.completeopt = "menuone,noselect"
-vim.opt.ignorecase = true
--- [[ Highlight on yank ]]
--- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
-vim.api.nvim_create_autocmd("TextYankPost", {
-	callback = function()
-		vim.highlight.on_yank()
-	end,
-	group = highlight_group,
-	pattern = "*",
-})
-vim.g.netrw_browse_split = 0
-vim.g.netrw_banner = 0
-vim.g.netrw_winsize = 25
+require("core.utils").load_mappings()
+
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+-- bootstrap lazy.nvim!
+if not vim.loop.fs_stat(lazypath) then
+  require("core.bootstrap").gen_chadrc_template()
+  require("core.bootstrap").lazy(lazypath)
+end
+
+dofile(vim.g.base46_cache .. "defaults")
+vim.opt.rtp:prepend(lazypath)
+require "plugins"
